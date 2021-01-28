a30d31dd7807401c2f0768c6925c9595016b1955
Eddie Bassey <eddie.bassey@nology.io>
2021-01-28 14:52:18 +0000

    Added faq section

--- src/Pages/AboutSection.js
+++ src/Pages/AboutSection.js
@@ -2,12 +2,14 @@ import React from "react";
 // Import components
 import AboutUs from "../components/AboutUs";
 import Services from "../components/Services";
+import Faq from "../components/Faq";
 
 const AboutSection = () => {
 	return (
 		<>
 			<AboutUs />
 			<Services />
+			<Faq />
 		</>
 	);
 };
