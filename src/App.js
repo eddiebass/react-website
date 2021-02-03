764f446313865cd61c0f95cfb65c6673c06081b1
Eddie Bassey <eddie.bassey@nology.io>
2021-02-03 23:38:20 +0000

    added navigation bar using react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
 // Import pages
 import AboutSection from "./Pages/AboutSection";
+import OurWork from "./Pages/OurWork";
+import ContactUs from "./Pages/ContactUs";
+
+import Nav from "./components/Nav";
 // import global style
 import GlobalStyle from "./components/GlobalStyle";
+//Import from react-router-dom
+import { Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div>
 			<GlobalStyle />
-			<AboutSection />
+			<Nav />
+			<Switch>
+				<Route path="/" exact>
+					<AboutSection />
+				</Route>
+				<Route path="/work">
+					<OurWork />
+				</Route>
+				<Route path="/contact">
+					<ContactUs />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
---
+++ src/Pages/OurWork.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OurWork = () => {
+	return <div>Our Work</div>;
+};
+
+export default OurWork;
