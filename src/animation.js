ca31bc65dfe9e750687cd1690c76213b3cc3e0a9
Eddie Bassey <eddie.bassey@nology.io>
2021-02-16 00:01:02 +0000

    added animation to our work page

--- src/animation.js
+++ src/animation.js
@@ -3,7 +3,11 @@ export const pageAnimation = {
 	show: {
 		opacity: 1,
 		y: 0,
-		transition: { duration: 1, when: "beforeChildren", staggerChildren: 0.25 },
+		transition: {
+			duration: 0.25,
+			when: "beforeChildren",
+			staggerChildren: 0.5,
+		},
 	},
 	exit: { opacity: 0, y: 300, transition: { duration: 0.5 } },
 };
@@ -26,6 +30,28 @@ export const photoAnimation = {
 	show: {
 		scale: 1,
 		opacity: 1,
-		transition: { ease: "easeOut", duration: 0.75 },
+		transition: { ease: "easeOut", duration: 0.75 /*delay: 0.5 */ },
 	},
+};
+
+export const lineAnimation = {
+	hidden: { width: "0%" },
+	show: {
+		width: "100%",
+		transition: { duration: 1 },
+	},
+};
+
+export const slider = {
+	hidden: { x: "-130%", skew: "45deg" },
+	show: {
+		x: "100%",
+		skew: "0deg",
+		transition: { ease: "easeOut", duration: 1 },
+	},
+};
+
+export const sliderContainer = {
+	hidden: { opacity: 1 },
+	show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
